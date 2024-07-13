b76f7d3783f93a2b930d65a061410380a94f1217
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-07-13 08:46:47 +0530

    2d Array

---
+++ TwoDArray/declareArray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// int main(){
+//     int arr[5][6];
+//     cout<<sizeof(arr);
+//     return 0;
+// }
+
+int main()
+{
+    int arr[4][5] = {
+        {1, 2, 3, 4, 5},
+        {2, 54, 6, 3, 6},
+        {4, 3, 5, 7, 8},
+        {8, 5, 9, 0, 5},
+    };
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[j][i] << " ";
+        }
+    }
+
+    return 0;
+}
